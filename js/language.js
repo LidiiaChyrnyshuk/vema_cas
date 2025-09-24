612bd9f3a40641116b715add3439bdf02cd2ffbc
LidiiaChyrnyshuk <l.mandych@gmail.com>
2025-09-24 16:18:17 +0200

    add tralslate-function

---
+++ js/language.js
@@ -0,0 +1,60 @@
+// Мапа перекладів
+const translations = {
+	en: {
+		title: "up to 6 000 EUR",
+		bonus: "+extra 50 FS",
+		heroButton: "Registration",
+		benefitsTitle: "Trusted Casino",
+		benefitsTitle2: "Gambling Guide for",
+		benefitsText: "30+ Years - Casino.org",
+		images: {
+			card1: "../images/benefits/CARD1.webp",
+			card2: "../images/benefits/CARD2.webp",
+			card3: "../images/benefits/CARD3.webp",
+		},
+		gamesTitle: "Over 8000+ games",
+		footerButton: "Play",
+	},
+	pt: {
+		title: "up to 6 000 EUR",
+		bonus: "+extra 50 FS",
+		heroButton: "Registration",
+		benefitsTitle: "Trusted Casino",
+		benefitsTitle2: "Gambling Guide for",
+		benefitsText: "30+ Years - Casino.org",
+		images: {
+			card1: "../images/benefits/CARD1.webp",
+			card2: "../images/benefits/CARD2.webp",
+			card3: "../images/benefits/CARD3.webp",
+		},
+		gamesTitle: "Over 8000+ games",
+		footerButton: "Play",
+	},
+};
+    function getBrowserLanguage() {
+			const lang = navigator.language || navigator.userLanguage;
+			if (lang.startsWith("pt")) return "pt";
+			return "en"; // fallback
+		}
+
+		function applyTranslations(lang) {
+			// Текст
+			document.querySelectorAll("[data-translate]").forEach((el) => {
+				const key = el.getAttribute("data-translate");
+				if (translations[lang][key]) {
+					el.textContent = translations[lang][key];
+				}
+			});
+
+			// Картинки
+			document.querySelectorAll("[data-translate-img]").forEach((el) => {
+				const key = el.getAttribute("data-translate-img");
+				if (translations[lang].images && translations[lang].images[key]) {
+					el.src = translations[lang].images[key];
+				}
+			});
+		}
+
+		// Виконуємо переклад при завантаженні сторінки
+		const lang = getBrowserLanguage();
+		applyTranslations(lang);
